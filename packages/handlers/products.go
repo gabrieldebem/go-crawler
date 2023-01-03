1db1420ca57ac7cbe77af1526c9671218a9163eb
Gabriel de Bem <gabrieldebem72@gmail.com>
2023-01-02 22:40:39 -0300

    customize product search

--- packages/handlers/products.go
+++ packages/handlers/products.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+	"fmt"
+	"sort"
+
 	"github.com/gabrieldebem/go-crawler/packages/contracts"
 	"github.com/gabrieldebem/go-crawler/packages/services"
 	"github.com/gin-gonic/gin"
@@ -10,12 +13,21 @@ var crawler contracts.ICrawler
 
 func GetProducts(c *gin.Context) {
 	crawler = services.MercadoLivreCrawler{}
-	url := "https://lista.mercadolivre.com.br/notebook#D[A:notebook]"
+	p := c.Query("product")
+	url := fmt.Sprintf("https://lista.mercadolivre.com.br/%s#D[A:%s]", p, p)
 	mProducts := crawler.Search(url)
 
+	sort.Slice(mProducts, func(i, j int) bool {
+		return mProducts[i].Price < mProducts[j].Price
+	})
+
 	crawler = services.AmazonCrawler{}
-	url = "https://www.amazon.com.br/s?k=notebook&__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3QPMLAG2FS1F9&sprefix=notebook%2Caps%2C236&ref=nb_sb_noss_1"
+	url = fmt.Sprintf("https://www.amazon.com.br/s?k=%s", p)
 	aProducts := crawler.Search(url)
+
+	sort.Slice(aProducts, func(i, j int) bool {
+		return aProducts[i].Price < aProducts[j].Price
+	})
 
 	c.JSON(200, gin.H{
 		"mercado_livre": mProducts,
